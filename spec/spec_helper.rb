42d13831c260d8743dab9fae0962d29e718507cd
Nick Nicholas <opoudjis@gmail.com>
2022-11-15 03:54:25 +1100

    remove rexml from rspec: https://github.com/metanorma/isodoc/issues/260

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@ end
 require "metanorma-standoc"
 require "rspec/matchers"
 require "equivalent-xml"
-require "rexml/document"
 require "byebug"
 
 Dir[File.expand_path("./support/**/**/*.rb", __dir__)].each { |f| require f }
