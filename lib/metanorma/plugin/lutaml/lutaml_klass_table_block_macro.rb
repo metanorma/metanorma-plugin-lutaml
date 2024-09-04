b7d6d9f8fd77f0515185d9d2d7f348663a0fad81
KW Kwan <kw.kwan@ribose.com>
2024-09-04 15:55:15 +0800

    Refactor code

--- lib/metanorma/plugin/lutaml/lutaml_klass_table_block_macro.rb
+++ lib/metanorma/plugin/lutaml/lutaml_klass_table_block_macro.rb
@@ -51,6 +51,16 @@ module Metanorma
             gen, parent, attrs
           )
 
+          # content[:inherited_props] += render_named_inherited_props(
+          #   gen, parent, attrs, "gml"
+          # )
+          # content[:inherited_props] += render_named_inherited_props(
+          #   gen, parent, attrs, "core"
+          # )
+          # content[:inherited_assoc_props] += render_named_inherited_props(
+          #   gen, parent, attrs, "gen"
+          # )
+
           rendered_table = render_table(gen, parent, attrs, content)
 
           block = create_open_block(parent, "", attrs)
@@ -69,31 +79,31 @@ module Metanorma
           table_tmpl.render
         end
 
-        def render_general_rows(gen_attributes, upper_klass, gen_name, parent,
-                                attrs)
+        def render_general_rows(general_item, parent, attrs)
           row_tmpl = get_template(
             parent.document, attrs["row_template"], :row
           )
-          render_rows(gen_attributes, upper_klass, gen_name, row_tmpl)
+          render_rows(general_item, row_tmpl)
         end
 
-        def render_assoc_rows(gen_attributes, upper_klass, gen_name, parent,
-                              attrs)
+        def render_assoc_rows(general_item, parent, attrs)
           row_tmpl = get_template(
             parent.document, attrs["assoc_template"], :assoc_row
           )
-          render_rows(gen_attributes, upper_klass, gen_name, row_tmpl)
+          render_rows(general_item, row_tmpl)
         end
 
-        def render_rows(gen_attributes, upper_klass, gen_name, row_tmpl)
-          attributes = prepare_row_attrbiutes(gen_attributes, upper_klass,
-                                              gen_name)
+        def render_rows(general_item, row_tmpl)
+          gen_attrs = general_item[:general_attributes]
+          upper_klass = general_item[:general_upper_klass]
+          gen_name = general_item[:general_name]
+          attributes = prepare_row_drops(gen_attrs, upper_klass, gen_name)
           row_tmpl.assigns["attributes"] = attributes
           row_tmpl.render
         end
 
-        def prepare_row_attrbiutes(gen_attributes, upper_klass, gen_name) # rubocop:disable Metrics/MethodLength
-          gen_attributes.map do |attr|
+        def prepare_row_drops(gen_attrs, upper_klass, gen_name) # rubocop:disable Metrics/MethodLength
+          gen_attrs.map do |attr|
             attr[:gen_name] = gen_name
             attr[:upper_klass] = upper_klass
             attr[:name_ns] = case attr[:type_ns]
@@ -110,82 +120,53 @@ module Metanorma
         end
 
         def render_owned_props(gen, parent, attrs)
-          gen_attributes = gen[:general_attributes]
-          upper_klass = gen[:general_upper_klass]
-          gen_name = gen[:general_name]
-          render_general_rows(gen_attributes, upper_klass, gen_name, parent,
-                              attrs)
+          render_general_rows(gen, parent, attrs)
         end
 
         def render_assoc_props(gen, parent, attrs)
-          gen_attributes = gen[:general_attributes]
-          upper_klass = gen[:general_upper_klass]
-          gen_name = gen[:general_name]
-          render_assoc_rows(gen_attributes, upper_klass, gen_name, parent,
-                            attrs)
-        end
-
-        def render_inherited_props(gen, parent, attrs)
-          rendered_rows = ""
-          # rendered_rows += render_gml_inherited_props(gen, parent, attrs)
-          # rendered_rows += render_core_inherited_props(gen, parent, attrs)
-
-          general_item = gen[:general]
-          while general_item && !general_item.empty?
-            gen_attributes = general_item[:general_attributes]
-            upper_klass = general_item[:general_upper_klass]
-            gen_name = general_item[:general_name]
-            rendered_rows += render_general_rows(gen_attributes, upper_klass,
-                                                 gen_name, parent, attrs)
-            general_item = general_item[:general]
-          end
-
-          rendered_rows
+          render_assoc_rows(gen, parent, attrs)
         end
 
         def render_gml_inherited_props(gen, parent, attrs)
-          rendered_rows = ""
-          rendered_rows += render_general_rows(
-            gen[:gml_attributes],
-            gen[:gml_attributes].first[:upper_klass],
-            "gml",
-            parent, attrs
-          )
-          rendered_rows
+          render_named_inherited_props(gen, parent, attrs, "gml")
         end
 
         def render_core_inherited_props(gen, parent, attrs)
-          rendered_rows = ""
-          rendered_rows += render_general_rows(
-            gen[:core_attributes],
-            gen[:core_attributes].first[:upper_klass],
-            "core",
-            parent, attrs
-          )
-          rendered_rows
+          render_named_inherited_props(gen, parent, attrs, "core")
         end
 
         def render_gen_inherited_props(gen, parent, attrs)
-          rendered_rows = ""
-          rendered_rows += render_general_rows(
-            gen[:gen_attributes],
-            gen[:gen_attributes].first[:upper_klass],
-            "gen",
-            parent, attrs
-          )
-          rendered_rows
+          render_named_inherited_props(gen, parent, attrs, "gen")
+        end
+
+        def render_named_inherited_props(gen, parent, attrs, name)
+          attr_key = "#{name}_attributes".to_sym
+          general_item = {}
+          general_item[:general_attributes] = gen[attr_key]
+          general_item[:general_upper_klass] = gen[attr_key].first[:upper_klass]
+          general_item[:general_name] = name
+
+          render_general_rows(general_item, parent, attrs)
+        end
+
+        def render_inherited_props(gen, parent, attrs)
+          render_inherited_rows(gen, parent, attrs, assoc: false)
         end
 
         def render_inherited_assoc_props(gen, parent, attrs)
+          render_inherited_rows(gen, parent, attrs, assoc: true)
+        end
+
+        def render_inherited_rows(gen, parent, attrs, assoc: false) # rubocop:disable Metrics/MethodLength
           rendered_rows = ""
-          # rendered_rows += render_gen_inherited_props(gen, parent, attrs)
           general_item = gen[:general]
+
           while general_item && !general_item.empty?
-            gen_attributes = general_item[:general_attributes]
-            upper_klass = general_item[:general_upper_klass]
-            gen_name = general_item[:general_name]
-            rendered_rows += render_assoc_rows(gen_attributes, upper_klass,
-                                               gen_name, parent, attrs)
+            rendered_rows += if assoc
+                               render_assoc_rows(general_item, parent, attrs)
+                             else
+                               render_general_rows(general_item, parent, attrs)
+                             end
             general_item = general_item[:general]
           end
 
