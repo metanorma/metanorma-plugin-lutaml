32b436407617be93a2c8f1b72d8b4cb39a493a5a
KW Kwan <kw.kwan@ribose.com>
2025-02-20 11:07:05 +0800

    Merge with lutaml_ea_xmi

--- lib/metanorma/plugin/lutaml/lutaml_uml_datamodel_description_preprocessor.rb
+++ lib/metanorma/plugin/lutaml/lutaml_uml_datamodel_description_preprocessor.rb
@@ -14,6 +14,7 @@ module Metanorma
   module Plugin
     module Lutaml
       #  Macro for quick rendering of datamodel attributes/values table
+      # TODO: merge with lutaml_ea_xmi
       class LutamlUmlDatamodelDescriptionPreprocessor <
           ::Asciidoctor::Extensions::Preprocessor
         include LutamlEaXmiBase
@@ -23,8 +24,11 @@ module Metanorma
 
         private
 
-        def parse_result_document(full_path, _guidance = nil)
-          ::Lutaml::Parser.parse(File.new(full_path, encoding: "UTF-8")).first
+        def parse_result_document(full_path, guidance)
+          ::Lutaml::XMI::Parsers::XML.serialize_xmi_to_liquid(
+            File.new(full_path, encoding: "UTF-8"),
+            guidance,
+          )
         end
       end
     end
