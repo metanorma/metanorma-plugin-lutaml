0c38bce4e05699fc8db3bd3baa112ffe0d168678
KW Kwan <kw.kwan@ribose.com>
2025-03-28 16:51:42 +0800

    Migrate YAML config files to use lutaml-model

---
+++ lib/metanorma/plugin/lutaml/config.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require "yaml"
+require "lutaml"
+require "lutaml/model"
+require_relative "config/package"
+require_relative "config/root"
+
+module Metanorma
+  module Plugin
+    module Lutaml
+      module Config
+      end
+    end
+  end
+end
