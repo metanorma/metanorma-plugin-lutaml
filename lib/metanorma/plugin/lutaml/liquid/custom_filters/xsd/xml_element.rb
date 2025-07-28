0f8011281b798123f9e57caca6f6d4612ddd81d6
suleman-uzair <suleman@workytical.com>
2025-07-28 21:50:48 +0500

    updated template helper methods

--- lib/metanorma/plugin/lutaml/liquid/custom_filters/xsd/xml_element.rb
+++ lib/metanorma/plugin/lutaml/liquid/custom_filters/xsd/xml_element.rb
@@ -32,6 +32,15 @@ module Metanorma
                 sequence_attrs(object)
               when ::Lutaml::Xsd::Element::ElementDrop
                 element_attrs(object)
+              when ::Lutaml::Xsd::Any::AnyDrop
+                any_attrs(object).map { |key, value| %( #{key}="#{value}") }.join
+              end
+            end
+
+            def children_xml_representation_for(object)
+              case object
+              when ::Lutaml::Xsd::SimpleContent::SimpleContentDrop
+                simple_content_children(object)
               end
             end
 
@@ -54,15 +63,50 @@ module Metanorma
               complex_type(object)
             end
 
-            def attr_attrs(object)
-              use = %( use="#{object.use}") if object.use
-              if object.ref
-                %( ref="#{object.ref}"#{use})
-              elsif object.name
-                fixed = %( fixed="#{object.fixed}") if object.fixed
-                default = %( default="#{object.default}") if object.default
-                %( name="#{object.name}" type="#{object.type}"#{use}#{fixed}#{default})
+            def simple_content_children(object)
+              tag_name = object.extension ? "extension" : "restriction"
+              instance = object.extension || object.restriction
+              base_tag_attrs = { base: instance.base }
+              [
+                xsd_open_tag(tag_name, base_tag_attrs),
+                resolved_element_order(instance).map { |child| "  #{simple_content_child(child)}" },
+                xsd_close_tag(tag_name),
+              ].flatten
+            end
+
+            def simple_content_child(child)
+              case child
+              when ::Lutaml::Xsd::Attribute::AttributeDrop
+                xsd_open_tag("attribute", attr_attrs_hash(child), closed: true)
+              when ::Lutaml::Xsd::AttributeGroup::AttributeGroupDrop
+                xsd_open_tag("attributeGroup", { ref: child.ref }, closed: true)
               end
+            end
+
+            def xsd_open_tag(tag_name, attributes = {}, closed: false)
+              attrs = attributes.map { |k, v| %(#{k}="#{v}") }.join(" ")
+              %(<xsd:#{tag_name} #{attrs}#{"/" if closed}>)
+            end
+
+            def xsd_close_tag(tag_name)
+              "</xsd:#{tag_name}>"
+            end
+
+            def attr_attrs(object)
+              attr_attrs_hash(object).map do |key, value|
+                %( #{key}="#{value}")
+              end.join
+            end
+
+            def attr_attrs_hash(object)
+              attrs = {}
+              attrs[:ref] = object.ref if object.ref
+              attrs[:name] = object.name if object.name
+              attrs[:type] = object.type if object.type
+              attrs[:fixed] = object.fixed if object.fixed
+              attrs[:use] = object.use if object.use
+              attrs[:default] = object.default if object.default
+              attrs
             end
 
             def sequence_attrs(object)
@@ -78,6 +122,16 @@ module Metanorma
               min_occurs = %( minOccurs="#{object.min_occurs}") if object.min_occurs
               max_occurs = %( maxOccurs="#{object.max_occurs}") if object.max_occurs
               %(#{ref}#{name}#{type}#{min_occurs}#{max_occurs}")
+            end
+
+            def any_attrs(object)
+              attrs = {}
+              attrs[:id] = object.id if object.id
+              attrs[:namespace] = object.namespace if object.namespace
+              attrs[:processContents] = object.process_contents if object.process_contents
+              attrs[:minOccurs] = object.min_occurs if object.min_occurs
+              attrs[:maxOccurs] = object.max_occurs if object.max_occurs
+              attrs
             end
           end
         end
