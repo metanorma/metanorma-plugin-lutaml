59502d78324a06e590ef3152f4e6fcbb8923d2db
KW Kwan <kw.kwan@ribose.com>
2025-08-13 16:28:09 +0800

    Add macro lutaml_enum_table

---
+++ lib/metanorma/plugin/lutaml/lutaml_enum_table_block_macro.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+module Metanorma
+  module Plugin
+    module Lutaml
+      class LutamlEnumTableBlockMacro <
+        ::Asciidoctor::Extensions::BlockMacroProcessor
+        include LutamlEaXmiBase
+
+        DEFAULT_TEMPLATE = File.join(
+          Gem::Specification.find_by_name("metanorma-plugin-lutaml").gem_dir,
+          "lib", "metanorma", "plugin", "lutaml", "liquid_templates",
+          "_enum_table.liquid"
+        )
+
+        use_dsl
+        named :lutaml_enum_table
+
+        def process(parent, target, attrs) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+          xmi_path = get_xmi_path(parent, target, attrs)
+
+          if attrs["template"]
+            attrs["template"] = Utils.relative_file_path(
+              parent.document, attrs["template"]
+            )
+          end
+
+          path = if !attrs["path"].nil?
+                   attrs["path"]
+                 elsif !attrs["package"].nil? && !attrs["name"].nil?
+                   "#{attrs['package']}::#{attrs['name']}"
+                 else
+                   attrs["name"]
+                 end
+
+          enum = ::Lutaml::XMI::Parsers::XML.serialize_enumeration_by_name(
+            xmi_path, path
+          )
+
+          render(enum, parent, attrs)
+        end
+
+        private
+
+        def render(enum, parent, attrs)
+          rendered_table = render_table(enum, parent, attrs)
+
+          block = create_open_block(parent, "", attrs)
+          parse_content(block, rendered_table, attrs)
+        end
+
+        def render_table(enum, parent, attrs)
+          table_tmpl = get_template(parent.document, attrs)
+          table_tmpl.assigns["enum"] = enum
+          table_tmpl.render
+        end
+
+        def get_template(document, attrs)
+          template = DEFAULT_TEMPLATE
+          if attrs["template"]
+            template = attrs["template"]
+          end
+
+          rel_tmpl_path = Utils.relative_file_path(
+            document, template
+          )
+
+          ::Liquid::Template.parse(File.read(rel_tmpl_path))
+        end
+      end
+    end
+  end
+end
