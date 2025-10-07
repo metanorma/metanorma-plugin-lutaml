101ed206d934394094c11c9d61679a3dd2cfa837
suleman-uzair <suleman@workytical.com>
2025-10-07 21:22:49 +0500

    updated filters module methods

--- lib/metanorma/plugin/lutaml/liquid/custom_filters/xsd/to_xml_representation.rb
+++ lib/metanorma/plugin/lutaml/liquid/custom_filters/xsd/to_xml_representation.rb
@@ -4,14 +4,10 @@ module Metanorma
       module Liquid
         module Xsd
           module CustomFilters
-            def to_xml_representation(element, skip_rendering = [])
-              ruby_object(element).to_xml(except: Array(skip_rendering&.to_sym))
-            end
-
-            private
-
-            def ruby_object(element)
-              element.instance_variable_get(:@object)
+            def to_xml_representation(element, skip_rendering = nil)
+              element.instance_variable_get(:@object).to_formatted_xml(
+                except: Array(skip_rendering&.to_sym),
+              )
             end
           end
         end
