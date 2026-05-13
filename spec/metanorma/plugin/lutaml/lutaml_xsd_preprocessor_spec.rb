9c44ade486f22013cefe340d5976f752de987127
Ronald Tse <ronald.tse@ribose.com>
2026-05-14 07:16:40 +0800

    spec: add UnitsML/OMML XSD rendering and direct block specs
    Add comprehensive specs covering:
    - UnitsML schema elements and complex types (block syntax)
    - OMML schema with location option (block syntax)
    - Full UnitsML documentation with cross-references (block syntax)
    - XSD direct block syntax with external Liquid templates
    - Mixed direct + block syntax in same document
    - Missing template file error handling
    
    Includes expected output XML fixtures for semantic comparison.

--- spec/metanorma/plugin/lutaml/lutaml_xsd_preprocessor_spec.rb
+++ spec/metanorma/plugin/lutaml/lutaml_xsd_preprocessor_spec.rb
@@ -176,5 +176,241 @@ RSpec.describe Metanorma::Plugin::Lutaml::LutamlXsdPreprocessor do
         ).reject(&:empty?)
       end
     end
+
+    context "with UnitsML schema elements and complex types" do
+      let(:unitsml_path) { fixtures_path("xsd_schemas/unitsml-v1.0-csd04.xsd") }
+
+      let(:input) do
+        <<~TEXT
+          = Document title
+
+          = Elements
+          [lutaml_xsd,#{unitsml_path},unitsml]
+          ----
+          {% for element in unitsml.element %}
+
+          Name: *{{ element.name }}*
+
+          Type: *{{ element.type }}*
+          {% endfor %}
+          ----
+
+          = ComplexTypes
+          [lutaml_xsd,#{unitsml_path},unitsml]
+          ----
+          {% for complex_type in unitsml.complex_type %}
+
+          Name: *{{ complex_type.name }}*
+
+          Description: {{ complex_type.annotation.documentation.first.content }}
+          {% endfor %}
+          ----
+        TEXT
+      end
+
+      let(:expected_output) do
+        File.read(fixtures_path("first-unitsml-expected-output-v1.0-csd04.xml"))
+      end
+
+      it "correctly renders input" do
+        expect_equivalent_document(
+          xml_string_content(metanorma_convert(input)),
+          expected_output,
+        )
+      end
+    end
+
+    context "with OMML schema elements and complex types" do
+      let(:omml_path) { fixtures_path("xsd_schemas/omml.xsd") }
+      let(:omml_dir) { File.dirname(omml_path) }
+
+      let(:input) do
+        <<~TEXT
+          = Document title
+
+          = Elements
+          [lutaml_xsd,#{omml_path},omml, location=#{omml_dir}]
+          ----
+          {% for element in omml.element %}
+
+          Name: *{{ element.name }}*
+          Type: *{{ element.type }}*
+          {% endfor %}
+          ----
+
+          = ComplexTypes
+          [lutaml_xsd,#{omml_path},omml]
+          ----
+          {% for complex_type in omml.complex_type %}
+
+          Name: *{{ complex_type.name }}*
+          Description: {{ complex_type.annotation.documentation.first.content }}
+          {% endfor %}
+          ----
+        TEXT
+      end
+
+      let(:expected_output) do
+        File.read(fixtures_path("minimal-unitsml-expected-output-v1.0-csd04.xml"))
+      end
+
+      it "correctly renders input" do
+        expect_equivalent_document(
+          xml_string_content(metanorma_convert(input)),
+          expected_output,
+        )
+      end
+    end
+
+    context "with UnitsML schema documentation via block syntax" do
+      let(:unitsml_path) { fixtures_path("xsd_schemas/unitsml-v1.0-csd04.xsd") }
+
+      let(:input) do
+        <<~TEXT
+          = Document title
+
+          [#top]
+          = Units Markup language (UnitsML) Schema Documentation unitsml-v1.0
+
+          = Elements
+          [lutaml_xsd,#{unitsml_path},unitsml]
+          ----
+          {% for element in unitsml.element %}
+
+          [#element_{{element.name}}]
+
+          *Element:* {{ element.name }}
+
+          *Type:* <<complex_type_{{element.type}},{{ element.type }}>>
+
+          *Description:* {{ element.annotation.documentation.first.content }}
+
+          <<#top, &#x2303;>>
+
+          ---
+          {% endfor %}
+
+          = Complex Types
+          {% for complex_type in unitsml.complex_type %}
+
+          [#complex_type_{{complex_type.name}}]
+
+          *Complex Type:* {{ complex_type.name }}
+
+          *Description:* {{ complex_type.annotation.documentation.first.content }}
+
+          <<#top, &#x2303;>>
+
+          ---
+          {% endfor %}
+
+          = Attribute Groups
+          {% for attribute_group in unitsml.attribute_group %}
+
+          [#attribute_group_{{attribute_group.name}}]
+
+          *Attribute Group:* {{ attribute_group.name }}
+
+          *Description:* {{ attribute_group.annotation.documentation.first.content }}
+
+          <<#top, &#x2303;>>
+
+          ---
+          {% endfor %}
+          ----
+        TEXT
+      end
+
+      let(:expected_output) do
+        File.read(fixtures_path("unitsml-expected-output-v1.0-csd04.xml"))
+      end
+
+      it "correctly renders input" do
+        expect_equivalent_document(
+          xml_string_content(metanorma_convert(input)),
+          expected_output,
+        )
+      end
+    end
+
+    context "with XSD direct block syntax (lutaml_xsd::)" do
+      let(:unitsml_path) do
+        fixtures_path("xsd_schemas/unitsml-v1.0-csd04.xsd")
+      end
+      let(:template_path) do
+        fixtures_path("unitsml_liquid_templates/schema.adoc")
+      end
+      let(:include_path) do
+        fixtures_path("unitsml_liquid_templates")
+      end
+
+      let(:input) do
+        <<~TEXT
+          = Document title
+
+          [#top]
+          = XSD Templates
+
+          lutaml_xsd::#{unitsml_path}[schema, #{template_path}, include_path=#{include_path}]
+        TEXT
+      end
+
+      let(:expected_output) do
+        File.read(fixtures_path("unitsml_expected.xml"))
+      end
+
+      it "correctly renders input using external Liquid template" do
+        expect_equivalent_document(
+          xml_string_content(metanorma_convert(input)),
+          expected_output,
+        )
+      end
+
+      context "with a missing template file" do
+        let(:input) do
+          <<~TEXT
+            = Document title
+
+            lutaml_xsd::#{unitsml_path}[schema, nonexistent.adoc]
+          TEXT
+        end
+
+        it "raises a file-not-found error" do
+          expect { metanorma_convert(input) }.to raise_error(Errno::ENOENT)
+        end
+      end
+
+      context "when a block header follows a direct block" do
+        let(:input) do
+          <<~TEXT
+            = Document title
+
+            lutaml_xsd::#{unitsml_path}[schema, #{template_path}, include_path=#{include_path}]
+
+            [lutaml_xsd,#{fixtures_path('xsd_schemas/person.xsd')},person]
+            ----
+            Person element count: {{ person.element.size }}
+            ----
+          TEXT
+        end
+
+        it "renders both syntaxes in the same document" do
+          rendered = xml_string_content(metanorma_convert(input))
+          expect(rendered).to include("Element:")
+          expect(rendered).to include("Person element count: 1")
+        end
+      end
+    end
+
+    def expect_equivalent_document(actual, expected)
+      expect(normalize_metanorma_header(actual))
+        .to(be_equivalent_to(normalize_metanorma_header(expected)))
+    end
+
+    def normalize_metanorma_header(xml)
+      xml
+        .sub(/(<metanorma\b[^>]*\bversion=)"[^"]+"/, '\1"_"')
+        .sub(%r{(<copyright>\s*<from>)\d{4}(</from>)}, '\1_\2')
+    end
   end
 end
